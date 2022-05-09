323bdbf214d9a8441488dd4d0a7f7cf738142a90
Ulrik Monrad <u.monrad@protonmail.com>
2022-05-09 20:48:21 +0200

    changes to lightspeed config and user keybindings after updates to plugin

--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -64,8 +64,8 @@ end
 
 M.set_lightspeed_keymaps = function()
   vim.cmd [[
-nmap s <Plug>Lightspeed_s
-nmap S <Plug>Lightspeed_S
+nmap s <Plug>Lightspeed_x
+nmap S <Plug>Lightspeed_X
 nmap <expr> f reg_recording() . reg_executing() == "" ? "<Plug>Lightspeed_f" : "f"
 nmap <expr> F reg_recording() . reg_executing() == "" ? "<Plug>Lightspeed_F" : "F"
 nmap <expr> t reg_recording() . reg_executing() == "" ? "<Plug>Lightspeed_t" : "t"
